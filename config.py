9ee2ecd3bcd8d5e807f5d4e907f026a01cbd195f
Kevin MacDonald <macdkw@gmail.com>
2022-11-02 17:22:32 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+api_key = "AIzaSyBAcb6v4Ee6EMhiCkH_Zc9OqRFeWgkLE7s"
+
+pg_host="ec2-54-147-36-107.compute-1.amazonaws.com"
+pg_database="d1i9m18bh0nee"
+pg_user="nrdsxccgistivu"
+pg_password="ef837c38415f990988b03d680674318e5eb625067b0fedbc7c4aa95c6154e0e7"
