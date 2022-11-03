62c3078ca665eeeb8f078999f5189d8ff55dd76e
Kevin MacDonald <macdkw@gmail.com>
2022-11-02 19:50:28 -0500

    done for the evening

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_key = "AIzaSyBAcb6v4Ee6EMhiCkH_Zc9OqRFeWgkLE7s"
+api_key = "AIzaSyAhZOLNLPIEGHOD1uo84iJ_DCYXx0q7eo0"
 
 pg_host="ec2-54-147-36-107.compute-1.amazonaws.com"
 pg_database="d1i9m18bh0nee"
