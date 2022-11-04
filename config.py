2766032ea3e78e652cdea70716522c4bdf9b2b8c
Kevin MacDonald <macdkw@gmail.com>
2022-11-04 11:51:48 -0500

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-api_key = "AIzaSyALmlAA17Twct--oKMT0l-caPIr0aBm6PU"
-# "AIzaSyBAcb6v4Ee6EMhiCkH_Zc9OqRFeWgkLE7s" = kwm.data
-# "AIzaSyAhZOLNLPIEGHOD1uo84iJ_DCYXx0q7eo0" = zedleppelin9
-# "AIzaSyALmlAA17Twct--oKMT0l-caPIr0aBm6PU" = macdkw
-
-pg_host="ec2-54-147-36-107.compute-1.amazonaws.com"
-pg_database="d1i9m18bh0nee"
-pg_user="nrdsxccgistivu"
-pg_password="ef837c38415f990988b03d680674318e5eb625067b0fedbc7c4aa95c6154e0e7"
