51f7ebd63a31cfe368e41596399b26780c0fded1
Kevin MacDonald <macdkw@gmail.com>
2022-11-10 17:03:09 -0600

    more

--- app2.py
+++ app2.py
@@ -36,8 +36,9 @@ category_options = [{'label': i, 'value': i} for i in categories]
 
 # create the sidebar
 sidebar = html.Div([
-    html.H3("YouTube Analysis", className='header',
+    html.H2("YouTube Analysis", className='header',
             style={'color': 'red', 'textAlign': 'center'}),
+            html.P('"Group 4" Final Project Dashboard for UT-Austin Data Analytics Bootcamp', style={'textAlign': 'center'}),
     html.Hr(),
     dbc.Nav(
         [
@@ -249,12 +250,60 @@ We chose 1 million views as our differentiating point of viral vs. not viral. Du
                          # Container for fourth section
                          html.Div(className='row', children=[
                              # Container for left side of fourth section
-                             html.Div(children=[
-                                 html.H5(
-                                     'Information about ML goes here'),
-                             ]),
-                         ])
+                             dcc.Markdown('''
+## Machine Learning Model
+We are using machine learning to see if we can predict whether or not a YouTube video can reach 1 million views based on its subscriber count, channel view count, channel video count, amount of comments on the video, amount of likes on the video, the topic of the video, and the day of week the video was published. We are using 1 million as our numeric value of whether the video has the potential to be 'viral' or not. Below is the code we used to add the viral or not column from our dataset we created using Youtube's API. 
+
+![code_for_viral_column](https://user-images.githubusercontent.com/105755095/201196386-c460bdf2-3feb-4ae8-824c-d409fd4be99e.png)
+
+![code_for_viral_col_2](https://user-images.githubusercontent.com/105755095/201196399-abcbed6b-ef87-45e9-9e08-0f5fac5feae8.png)
+
+### Preprocessing Data
+In order to begin the Machine Learning portion, we must preprocess the feature set of the data. First we encoded the categorical data. We did this for the topic category column and the day of week published column. This turned the categorical values into numerical values. However, for the day of week published column, we decided to go with custom encoding so that the days of week were not assigned random numbers, but instead 1 started with Sunday and 7 was Saturday. You can see the code we used below to encode the columns and then add custom encoding to the last column. 
+
+![encoding](https://user-images.githubusercontent.com/105755095/201197005-4c819df7-2ba3-4332-9a6a-a1cda5c92744.png)
+
+![custom_encoding](https://user-images.githubusercontent.com/105755095/201197039-dfad346b-38bf-4505-9b05-22d1e5e77de9.png)
+
+![final_custom_encoding](https://user-images.githubusercontent.com/105755095/201197053-f6656825-f06e-40af-9297-fc0e43d008ca.png)
+
+#### Scaling the Data
+Because our columns had some very large numbers, we thought it would be best to scale each column so that every column was on the same playing field. We did this with Standard Scaler from the sklearn package. This standardizes a feature by subtracting the mean and then scaling to unit variance. Unit variance means dividing all the values by the standard deviation. 
+
+![Screen Shot 2022-11-09 at 5 43 19 PM](https://user-images.githubusercontent.com/105755095/201197525-6971f879-26dd-468c-8fb8-8028bbeb0dc8.png)
+
+![scaled_for_ml](https://user-images.githubusercontent.com/105755095/201197546-0f755aef-017f-43a7-9682-0d0374f87dff.png)
+
+### Feature Selection
+We selected the features for our machine learning by deciding what data points would effect how much a video is viewed. The first thing we thought of was the channel that posted the video. If the channel has many subscribers than the potential for the video being viewed increases. This is the same thought process we had for the total views the channel has ever had. The more views a channel has had can give a good idea that they are going to continue getting many views. But this can be affected by the amount of videos on their channel. We assumed that the more videos a channel posts, there is a potential that they one of those videos will get many views. 
+
+We also thought categorical data was important to include in our feature selection process. We included the topic category of the channel because some topics may get more views than others. Same goes for the day of week the video was published. We wanted to see if there was a correlation between when a video was published to the amount of views that the video will get. 
+
+The video data itself also seemed like a legitiment feature to include as the amount of comments a video is getting and being talked about can affect the amount of views a video gets. And finally, we felt as though the like count on a video can be a predictor of whether or not the video will gain attraction. 
+
+![Correlation_Matrix](https://user-images.githubusercontent.com/105755095/201199625-ac2f5d9f-c37f-4db8-83ab-1cd9e040fe10.png)
+
+Above shares how the numerical features correlate with one another. 
+
+### Splitting Data
+We split the data using sklearn's train_test_split function. This split our data as the default 80% for training and 20% for testing.
+
+![train_test_split](https://user-images.githubusercontent.com/105755095/201200037-0217adac-3078-45ac-88c5-94e5fff53e3d.png)
+
+We used this split the first time around for our machine learning portion, before we remebered that our data might be imbalanced. So with the value count function, we checked how many of the rows were given the 1 for 'viral' and how many were given the 0 for 'not viral' and this is what was shown:
+
+![y_value_count](https://user-images.githubusercontent.com/105755095/201200265-1825b3d8-815a-426a-ac93-cfa2c2e8b38a.png)
+
+Our data was very imbalanced and we needed to use a function that would balance this data. Our instructor suggested we use SMOTEENN as it does both undersampling and oversampling at the same time. After resampling our data, we rechecked the y values and the numbers we much more balanced than before. 
+
+![smoteenn_code](https://user-images.githubusercontent.com/105755095/201200693-6b314a72-9f8f-4cd7-b1aa-7cf092287b8d.png)
+
+### Model Choice
+Our group choice 3 different machine learning models for our dataset. ''', style={'margin-right': '2%'}),
                          ]),
+                         ]),
+
+
 # ------------------------------------------------------------
 # Top Channels
     elif pathname == "/page-5":
@@ -298,10 +347,11 @@ We chose 1 million views as our differentiating point of viral vs. not viral. Du
     elif pathname == "/page-6":
         # imbed tableau dashboard
         return html.Div([html.H3(children='Tableau Dashboard'),
-                            html.Hr(),
-                            # display tableau.html
-                            html.Iframe(srcDoc=open('tableau.html', 'r').read(), width='100%', height='1000')
-                            ])
+                         html.Hr(),
+                         # display tableau.html
+                         html.Iframe(srcDoc=open(
+                             'tableau.html', 'r').read(), width='100%', height='1000')
+                         ])
 # ------------------------------------------------------------
 # Error404
     return html.Div(
@@ -312,6 +362,7 @@ We chose 1 million views as our differentiating point of viral vs. not viral. Du
         ],
         className="p-3 bg-light rounded-3",
     )
+
 
 @app.callback(
     Output('bar-chart', 'figure'),
@@ -337,6 +388,7 @@ def update_graph(checklist, xaxis):
     fig.update_traces(marker_color='red')
     return fig
 
+
 @app.callback(
     Output('top-channels', 'figure'),
     [Input('yaxis', 'value')],)
@@ -345,6 +397,7 @@ def update_graph(yaxis):
                  y="custom_url", x=yaxis, title='Top Channels', color='topic_category', template=template, height=600, width=1000)
     fig.update_layout(yaxis_categoryorder='total ascending')
     return fig
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
